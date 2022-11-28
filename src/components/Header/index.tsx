3efcd8e6cb9fc48b072653618e9b803b1f49916b
Ian Torquato <iantorquato2@gmail.com>
2022-11-28 20:05:50 -0300

    ✨ Added basic repository search

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
 import { Input, Layout, Typography } from 'antd'
+import { githubApi } from '../../core/api.github'
 
 import styles from './header.module.css'
 
 type HeaderProps = {}
 
 export function Header({}: HeaderProps) {
+  async function handleSearch(params: string) {
+    const [owner, repo] = params.split('/')
+
+    const data = await githubApi.repos.get({ owner, repo })
+
+    console.log(data)
+  }
+
   return (
     <Layout.Header className={styles.header}>
       <Typography.Title level={1}>Repository Viability</Typography.Title>
 
-      <Input.Search size='large' placeholder='Buscar repositório' />
+      <Input.Search size='large' placeholder='Buscar repositório: owner/repo' onSearch={handleSearch} enterButton />
     </Layout.Header>
   )
 }
