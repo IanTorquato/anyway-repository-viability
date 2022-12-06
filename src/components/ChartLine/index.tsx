1f5dd4875164a389388cb6bc11f971333725e3fa
Davi Fernandes <davifernandes507@gmail.com>
2022-12-06 19:57:09 -0300

    ✨ Added `ChartLine` component

---
+++ src/components/ChartLine/index.tsx
@@ -0,0 +1,73 @@
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+import {
+  commitsByWeekAngular,
+  commitsByWeekJquery,
+  commitsByWeekReact,
+  commitsByWeekSvelte,
+  commitsByWeekVue,
+  weekSerialized,
+} from '../../mocks/commitsByWeek';
+import { ChartCard } from '../ChartCard';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+export function ChartLine() {
+  return (
+    <ChartCard
+      content={
+        <Line
+          data={{
+            labels: weekSerialized,
+            datasets: [
+              {
+                label: 'React',
+                data: commitsByWeekReact.map(({ total }) => total),
+                borderColor: '#61dafb',
+                backgroundColor: '#61dafb',
+              },
+              {
+                label: 'Angular',
+                data: commitsByWeekAngular.map(({ total }) => total),
+                borderColor: '#bb002e',
+                backgroundColor: '#bb002e',
+              },
+              {
+                label: 'Vue',
+                data: commitsByWeekVue.map(({ total }) => total),
+                borderColor: '#3eaf7c',
+                backgroundColor: '#3eaf7c',
+              },
+              {
+                label: 'Jquery',
+                data: commitsByWeekJquery.map(({ total }) => total),
+                borderColor: '#1064a5',
+                backgroundColor: '#1064a5',
+              },
+              {
+                label: 'Svelte',
+                data: commitsByWeekSvelte.map(({ total }) => total),
+                borderColor: '#f23b00',
+                backgroundColor: '#f23b00',
+              },
+            ],
+          }}
+          options={{
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top' as const,
+              },
+              title: {
+                display: true,
+              },
+            },
+          }}
+        />
+      }
+      title="Quantidade de commits no ultimo ano"
+    />
+  );
+}
