cad7f4f4e74866926891ee0b347fd3414439b448
Ian Torquato <iantorquato2@gmail.com>
2022-12-06 22:27:04 -0300

    ✨ Added chart caches

--- src/components/ChartLine/index.tsx
+++ src/components/ChartLine/index.tsx
@@ -1,83 +1,76 @@
-import { Col, Row, Skeleton, Spin } from 'antd';
+import { Col, Row } from 'antd';
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 
-import { useFetch } from '../../hooks/useFetch';
 import { ChartCard } from '../ChartCard';
 
 import { CommitByWeekType } from './types';
 import { serializedWeeks } from './utils/serializedWeeks';
 
-export function ChartLine() {
-  const { data: commitsByWeekReact, loading: loadingReact } = useFetch<CommitByWeekType[]>('facebook/react/stats/commit_activity');
-  const { data: commitsByWeekJquery, loading: loadingJquery } = useFetch<CommitByWeekType[]>('jquery/jquery/stats/commit_activity');
-  const { data: commitsByWeekAngular, loading: loadingAngular } = useFetch<CommitByWeekType[]>('angular/angular/stats/commit_activity');
-  const { data: commitsByWeekSvelte, loading: loadingSvelte } = useFetch<CommitByWeekType[]>('sveltejs/svelte/stats/commit_activity');
-  const { data: commitsByWeekVue, loading: loadingVue } = useFetch<CommitByWeekType[]>('vuejs/vue/stats/commit_activity');
+export type ChartLineProps = {
+  commitsByWeekReact: CommitByWeekType[];
+  commitsByWeekJquery: CommitByWeekType[];
+  commitsByWeekAngular: CommitByWeekType[];
+  commitsByWeekSvelte: CommitByWeekType[];
+  commitsByWeekVue: CommitByWeekType[];
+};
 
+export function ChartLine({ commitsByWeekReact, commitsByWeekJquery, commitsByWeekAngular, commitsByWeekSvelte, commitsByWeekVue }: ChartLineProps) {
   const commitsByWeek = commitsByWeekReact && Array.isArray(commitsByWeekReact) ? serializedWeeks(commitsByWeekReact.map(({ week }) => week)) : undefined;
-
-  const loading = loadingReact || loadingJquery || loadingAngular || loadingSvelte || loadingVue;
 
   return (
     <Row gutter={[24, 24]} style={{ width: '100%', margin: '16px 0' }}>
       <Col span={24}>
         <ChartCard
           content={
-            <Spin spinning={loading}>
-              {loading ? (
-                <Skeleton />
-              ) : (
-                <Line
-                  data={{
-                    labels: commitsByWeek,
-                    datasets: [
-                      {
-                        label: 'React',
-                        data: Array.isArray(commitsByWeekReact) ? commitsByWeekReact?.map(({ total }) => total) : undefined,
-                        borderColor: '#61dafb',
-                        backgroundColor: '#61dafb',
-                      },
-                      {
-                        label: 'Angular',
-                        data: Array.isArray(commitsByWeekReact) ? commitsByWeekAngular?.map(({ total }) => total) : undefined,
-                        borderColor: '#bb002e',
-                        backgroundColor: '#bb002e',
-                      },
-                      {
-                        label: 'Vue',
-                        data: Array.isArray(commitsByWeekReact) ? commitsByWeekVue?.map(({ total }) => total) : undefined,
-                        borderColor: '#3eaf7c',
-                        backgroundColor: '#3eaf7c',
-                      },
-                      {
-                        label: 'Jquery',
-                        data: Array.isArray(commitsByWeekReact) ? commitsByWeekJquery?.map(({ total }) => total) : undefined,
-                        borderColor: '#1064a5',
-                        backgroundColor: '#1064a5',
-                      },
-                      {
-                        label: 'Svelte',
-                        data: Array.isArray(commitsByWeekReact) ? commitsByWeekSvelte?.map(({ total }) => total) : undefined,
-                        borderColor: '#f23b00',
-                        backgroundColor: '#f23b00',
-                      },
-                    ],
-                  }}
-                  options={{
-                    responsive: true,
-                    plugins: {
-                      legend: {
-                        position: 'top' as const,
-                      },
-                      title: {
-                        display: true,
-                      },
-                    },
-                  }}
-                />
-              )}
-            </Spin>
+            <Line
+              data={{
+                labels: commitsByWeek,
+                datasets: [
+                  {
+                    label: 'React',
+                    data: Array.isArray(commitsByWeekReact) ? commitsByWeekReact?.map(({ total }) => total) : undefined,
+                    borderColor: '#61dafb',
+                    backgroundColor: '#61dafb',
+                  },
+                  {
+                    label: 'Angular',
+                    data: Array.isArray(commitsByWeekReact) ? commitsByWeekAngular?.map(({ total }) => total) : undefined,
+                    borderColor: '#bb002e',
+                    backgroundColor: '#bb002e',
+                  },
+                  {
+                    label: 'Vue',
+                    data: Array.isArray(commitsByWeekReact) ? commitsByWeekVue?.map(({ total }) => total) : undefined,
+                    borderColor: '#3eaf7c',
+                    backgroundColor: '#3eaf7c',
+                  },
+                  {
+                    label: 'Jquery',
+                    data: Array.isArray(commitsByWeekReact) ? commitsByWeekJquery?.map(({ total }) => total) : undefined,
+                    borderColor: '#1064a5',
+                    backgroundColor: '#1064a5',
+                  },
+                  {
+                    label: 'Svelte',
+                    data: Array.isArray(commitsByWeekReact) ? commitsByWeekSvelte?.map(({ total }) => total) : undefined,
+                    borderColor: '#f23b00',
+                    backgroundColor: '#f23b00',
+                  },
+                ],
+              }}
+              options={{
+                responsive: true,
+                plugins: {
+                  legend: {
+                    position: 'top' as const,
+                  },
+                  title: {
+                    display: true,
+                  },
+                },
+              }}
+            />
           }
           title="Quantidade de commits no ultimo ano"
         />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,63 @@
 import { Layout } from 'antd';
+import { AxiosResponse } from 'axios';
 import { GetStaticPropsResult } from 'next';
 
 import { ChartBar, ChartBarProps } from '../components/ChartBar';
-import { ChartLine } from '../components/ChartLine';
+import { ChartLine, ChartLineProps } from '../components/ChartLine';
 import { Header } from '../components/Header';
 import { api } from '../core/api';
 
 const timeToRevalidate = 60 * 15; // In seconds - 15m
 
-export default function Home(props: ChartBarProps) {
+type HomeProps = {
+  barProps: ChartBarProps;
+  lineProps: ChartLineProps;
+};
+
+export default function Home({ barProps, lineProps }: HomeProps) {
   return (
     <Layout style={{ height: '100%' }}>
       <Header />
 
-      {/* <ul>
-        <li>name: {repository?.name}</li>
-        <li>full_name: {repository?.full_name}</li>
-        <li>description: {repository?.description}</li>
-        <li>stargazers_count: {repository?.stargazers_count}</li>
-        <li>watchers_count: {repository?.watchers_count}</li>
-        <li>language: {repository?.language}</li>
-        <li>open_issues_count: {repository?.open_issues_count}</li>
-      </ul> */}
-
-      <ChartBar {...props} />
-
-      <ChartLine />
+      <ChartBar {...barProps} />
+      <ChartLine {...lineProps} />
     </Layout>
   );
 }
 
 export async function getStaticProps() {
-  const { data: dataReact } = await api.get('/facebook/react');
-  const { data: dataJquery } = await api.get('/jquery/jquery');
-  const { data: dataAngular } = await api.get('/angular/angular');
-  const { data: dataSvelte } = await api.get('/sveltejs/svelte');
-  const { data: dataVue } = await api.get('/vuejs/vue');
+  const repositoryPaths = [
+    '/facebook/react',
+    '/jquery/jquery',
+    '/angular/angular',
+    '/sveltejs/svelte',
+    '/vuejs/vue',
+    'facebook/react/stats/commit_activity',
+    'jquery/jquery/stats/commit_activity',
+    'angular/angular/stats/commit_activity',
+    'sveltejs/svelte/stats/commit_activity',
+    'vuejs/vue/stats/commit_activity',
+  ];
+
+  const responses = await Promise.all<AxiosResponse>(repositoryPaths.map(path => api.get(path)));
+
+  const dataReact = responses[0].data;
+  const dataJquery = responses[1].data;
+  const dataAngular = responses[2].data;
+  const dataSvelte = responses[3].data;
+  const dataVue = responses[4].data;
+
+  const commitsByWeekReact = responses[5].data;
+  const commitsByWeekJquery = responses[6].data;
+  const commitsByWeekAngular = responses[7].data;
+  const commitsByWeekSvelte = responses[8].data;
+  const commitsByWeekVue = responses[9].data;
 
   return {
-    props: { dataReact, dataJquery, dataAngular, dataSvelte, dataVue },
+    props: {
+      barProps: { dataReact, dataJquery, dataAngular, dataSvelte, dataVue },
+      lineProps: { commitsByWeekReact, commitsByWeekJquery, commitsByWeekAngular, commitsByWeekSvelte, commitsByWeekVue },
+    },
     revalidate: timeToRevalidate,
-  } as GetStaticPropsResult<ChartBarProps>;
+  } as GetStaticPropsResult<HomeProps>;
 }
